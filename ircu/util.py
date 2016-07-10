55d9da44e71985e8fa81ffa60ea07f6db8c5e81e
Brian Cline <brian@ircplanet.net>
2016-07-10 00:44:29 -0500

    Add server/user numeric conversion methods

--- ircu/util.py
+++ ircu/util.py
@@ -3,7 +3,7 @@ from ircu import consts
 
 def int_to_base64(n, count):
     buf = ''
-    while count > 0:
+    while count:
         count -= 1
         buf = consts.BASE64_INT_TO_NUM[n & consts.BASE64_NUMNICKMASK] + buf
         n >>= consts.BASE64_NUMNICKLOG
@@ -16,3 +16,19 @@ def base64_to_int(s):
         n = n << 6
         n += consts.BASE64_NUM_TO_INT[ord(s[ii])]
     return n
+
+
+def server_num_str(s):
+    return int_to_base64(s, consts.BASE64_SERVLEN)
+
+
+def user_num_str(s):
+    return int_to_base64(s, consts.BASE64_USERLEN)
+
+
+def server_num_int(s):
+    return base64_to_int(s)
+
+
+def user_num_int(s):
+    return base64_to_int(s)
